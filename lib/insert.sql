1db9ce31c3b4898179529ab5701b04c67efc6941
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-16 19:57:46 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Mr. Chocolate", 10, "M", "Black", 1)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Rowdy", 7, "M", "Brown", 0)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Tabitha", 12, "F", "Brown", "1")
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", 0)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Melissa", 17, "F", "Black", 0)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Grinch", 4, "M", "Brown", 1)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES ("Wendy", 8, "F", "Black", 1)
+INSERT INTO bears (name, age, gender, color, temerament, alive) VALUES (null, 17, "F", "Black", 1)
